18c91444fc297db396125a23dcc0b42da398a6b9
Patrick Kunka <patrick@wearecolony.com>
2015-09-16 18:50:10 +0100

    parsing log files into objects

--- main.js
+++ main.js
@@ -1,106 +1,220 @@
 var AWS         = require('aws-sdk'),
-    s3          = new AWS.S3(),
-    Wacalytics  = null,
-    q           = require('q'),
     zlib        = require('zlib'),
-    fs          = require('fs');
+    fs          = require('fs'),
+    q           = require('q'),
 
-Wacalytics = function() {
+    wacalytics  = null,
+    s3          = null;
 
-};
+s3 = new AWS.S3();
 
-Wacalytics.prototype = {
-    constructor: Wacalytics,
+/**
+ * wacalytics
+ * @singleton
+ */
 
-    parseLogFile: function(gzipBuffer) {
+wacalytics = {
+
+    /**
+     * writeToDb
+     * @param {Event[]} events
+     */
+
+    writeToDb: function(events) {
+        console.log('[wacalytics] Writing ' + events.length + ' events to DB...');
+
+        // Write to DB ...
+    },
+
+    /**
+     * parseEventData
+     * @param {String} query
+     * @return {Object}
+     */
+
+    parseEventData: function(query) {
+        var output = {},
+            pairs = [];
+
+        if (query === '-') {
+            return output;
+        }
+
+        pairs = query.split('&');
+
+        pairs.forEach(function(pair) {
+            var bits = pair.split('='),
+                key = bits[0],
+                value = bits[1];
+
+            output[key] = value;
+        });
+
+        return output;
+    },
+
+    /**
+     * parseLogFile
+     * @param {String} logData
+     * @return {Event[]}
+     */
+
+    parseLogFile: function(logData) {
         var self = this,
-            defered = q.defer();
+            rows = logData.split('\n'),
+            keys = rows[1].split(' '),
+            events = [],
+            event = null;
 
-        console.log('buffer in: ', gzipBuffer);
+        keys.shift();
 
-        zlib.unzip(gzipBuffer, function(err, result) {
-            if (err) {
-                return defered.reject();
+        rows.shift();
+        rows.shift();
+        rows.pop();
+
+        console.log('[wacalytics] Found ' + rows.length + ' events');
+
+        rows.forEach(function(row) {
+            var fields = row.split('	');
+
+            event = {};
+
+            keys.forEach(function(key, i) {
+                event[key] = fields[i];
+            });
+
+            event.data = self.parseEventData(event['cs-uri-query']);
+
+            events.push(event);
+        });
+
+        return events;
+    },
+
+    /**
+     * unzipLogFile
+     * @param {Buffer} gzipBUffer
+     * @return {Promise} -> {String}
+     */
+
+    unzipLogFile: function(gzipBuffer) {
+        var defered = q.defer(),
+            logData = '';
+
+        zlib.unzip(gzipBuffer, function(e, buffer) {
+            if (e) {
+                return defered.reject(e);
             }
 
-            console.log('buffer out: ', result);
-
-            buffer   = new Buffer(result, 'base64');
+            defered.resolve(buffer.toString());
         });
 
         return defered.promise;
     },
 
+    /**
+     * readFile
+     * @param {String} srcBucket
+     * @param {String} srcKey
+     * @return {Promise} -> {Buffer}
+     */
+
     readFile: function(srcBucket, srcKey) {
-        var defer = q.defer();
+        var defered = q.defer();
 
-        console.log('Reading file ...');
+        if (typeof AWS_ENVIRONMENT !== 'undefined') {
+            // AWS
 
-        // s3.getObject({
-        //     Bucket: srcBucket,
-        //     Key: srcKey
-        // },
-        // function(err, response) {
-        //     if (err) {
-        //         return defer.reject(err);
-        //     }
+            console.log('[wacalytics] Detected AWS environment');
 
-        //     defer.resolve(response);
-        // });
+            s3.getObject({
+                Bucket: srcBucket,
+                Key: srcKey
+            },
+            function(e, file) {
+                if (e) {
+                    return defered.reject(e);
+                }
 
-        fs.readFile('bucket/log.gz', function (err, data) {
-            if (err) throw err;
-
-            zlib.unzip(data, function(err, result) {
-                console.log(result.toString());
-
-                // fs.writeFile('bucket/test.txt', result);
-            });
-        });
-
-        return defer.promise;
-    },
-
-    handlePut: function(record) {
-        var self = this,
-            srcBucket = record.s3.bucket.name,
-            srcKey    = decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '));
-
-        console.log(srcBucket, srcKey);
-
-        return self.readFile(srcBucket, srcKey)
-            .then(function(file) {
                 if (file.Body) {
                     switch (file.ContentType) {
                         case 'application/x-gzip':
-                            return self.parseLogFile(file.Body);
+                            defered.resolve(file.Body);
 
                             break;
                         default:
-                            console.log('Unrecognised file type', file.ContentType);
+                            console.log('[wacalytics] Unrecognised file type', file.ContentType);
                     }
                 }
             });
+        } else {
+            // Local development
+
+            console.log('[wacalytics] Detected local dev environment');
+
+            fs.readFile('bucket/log.gz', function (e, buffer) {
+                if (e) {
+                    defered.reject(e);
+                }
+
+                defered.resolve(buffer);
+            });
+        }
+
+        return defered.promise;
     },
+
+    /**
+     * handlePut
+     * @param {Record} record
+     * @return {Promise}
+     */
+
+    handlePut: function(record) {
+        var self        = this,
+            srcBucket   = record.s3.bucket.name,
+            srcKey      = decodeURIComponent(record.s3.object.key.replace(/\+/g, ' '));
+
+        return self.readFile(srcBucket, srcKey)
+            .then(function(buffer) {
+                return self.unzipLogFile(buffer);
+            })
+            .then(function(logData) {
+                return self.parseLogFile(logData);
+            })
+            .then(function(events) {
+                return self.writeToDb(events);
+            });
+    },
+
+    /**
+     * eventBus
+     * @param {Record} record
+     * @return {Promise}
+     */
 
     eventBus: function(record) {
         var self = this;
 
         switch (record.eventName) {
             case 'ObjectCreated:Put':
-                console.log('File "PUT" event');
-
                 return self.handlePut(record);
 
                 break;
             default:
-                console.log('Unrecognised event');
+                console.log('[wacalytics] Unrecognised event "' + record.eventName + '"');
         }
     },
 
-    init: function(event) {
-        var self = this,
-            tasks = [];
+    /**
+     * handleEvent
+     * @param {Event} event
+     * @return {Promise}
+     */
+
+    handleEvent: function(event) {
+        var self    = this,
+            tasks   = [];
 
         event.Records.forEach(function(event) {
             tasks.push(self.eventBus(event));
@@ -110,12 +224,12 @@ Wacalytics.prototype = {
     }
 };
 
-// For development/testing purposes
 exports.handler = function(event, context) {
-    var wac = new Wacalytics();
-
-    wac.init(event)
+    wacalytics.handleEvent(event)
         .then(function() {
             context.done();
+        })
+        .catch(function(e) {
+            console.error(e.stack);
         });
-}
+};
