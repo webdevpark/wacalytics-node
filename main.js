1f7f2a445fc55c7a0146e6f7d08dafa29cc533d2
Patrick Kunka <patrick@wearecolony.com>
2015-09-16 17:15:42 +0100

    read file from s3

--- main.js
+++ main.js
@@ -1,10 +1,69 @@
+var AWS         = require('aws-sdk'),
+    s3          = new AWS.S3(),
+    Wacalytics  = null,
+    q           = require('q');
+
+Wacalytics = function() {
+
+};
+
+Wacalytics.prototype = {
+    constructor: Wacalytics,
+
+    readFile: function(srcBucket, srcKey) {
+        var defer = q.defer();
+
+        s3.getObject({
+            Bucket: srcBucket,
+            Key: srcKey
+        },
+        function(response) {
+            defer.resolve(response);
+        });
+
+        return defer.promise;
+    },
+
+    handlePut: function(record) {
+        var self = this,
+            srcBucket = record.s3.bucket.name,
+            srcKey    = decodeURIComponent(record.s3.object.key.replace(/\+/g, " "));
+
+        return self.readFile(srcBucket, srcKey)
+            .then(function(response) {
+                console.log(response);
+            });
+    },
+
+    eventBus: function(record) {
+        var self = this;
+
+        switch (record.eventName) {
+            case 'ObjectCreated:Put':
+                return self.handlePut(record);
+
+                break;
+        }
+    },
+
+    init: function(event) {
+        var self = this,
+            tasks = [];
+
+        event.Records.forEach(function(event) {
+            tasks.push(self.eventBus(event));
+        });
+
+        return q.all(tasks);
+    }
+};
+
 // For development/testing purposes
 exports.handler = function(event, context) {
-  console.log( "Running main.handler" );
-  console.log( "==================================");
-  console.log( "event", event );
-  console.log( "==================================");
-  console.log( "Stopping main.handler" );
+    var wac = new Wacalytics();
 
-  context.done( );
+    wac.init(event)
+        .then(function() {
+            context.done();
+        });
 }
